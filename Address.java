0bf871e5444482d756b34ad48b74300379d01739
Kenneth Zepeda <33753783+zepedak1@users.noreply.github.com>
2019-10-30 13:41:20 -0400

    Update Address.java

--- Address.java
+++ Address.java
@@ -3,7 +3,7 @@ public class Address
     public static void main(String[] args)
     {
 	System.out.println("Kenneth Zepeda");
-	System.out.println("26 North 9th Ave");
-	System.out.println("Manville, New Jersey, 08835");
+	System.out.println("Street Address");
+	System.out.println("City, State, Zip Code");
     }
 }
